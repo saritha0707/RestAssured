e18320cae0da93bcdbcda166b1e86e403b291fae
schil30@safeway.com <Saritha.Chilakalapudi@albertsons.com>
2025-07-21 17:38:41 +0530

    main

--- src/main/feature/GET_request.feature
+++ src/main/feature/GET_request.feature
@@ -1,12 +1,14 @@
 Feature:Basic Rest Assured Practice
 
   #/employees{id} - get method to get employee details
+  @tag1
   Scenario: Validate GET method in Rest Assured
     Given I create a request "http://dummy.restapiexample.com/api/v1"
     When I send request with body to server "/employees"
     Then I validate response Status Code 200
 
-   Scenario: Validate GET method response parameters
+  @tag1
+  Scenario: Validate GET method response parameters
      Given I create a request "http://restapi.demoqa.com/utilities/weather/city"
      When I send request with body to server "/Hyderabad"
      Then I validate response parameters
---
+++ src/test/java/runners/RunCucumberTest.java
@@ -0,0 +1,18 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {
+    "pretty", // Prints readable output in the console
+    "json:target/cucumber-report.json", // JSON report for integration with other tools
+    "html:target/cucumber-html-reports/cucumber-html-reports.html", // HTML report
+    "junit:target/cucumber-report/cucumber.xml" // JUnit XML report
+}, glue = {"cost.steps"}, // Path to step definitions
+        features = "src/test/resources/features", // Path to feature files
+        tags = "not @ignore" // Exclude scenarios tagged with @ignore
+)
+public class RunCucumberTest {
+}
